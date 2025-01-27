16ca915068a820c997d3faf700c36ce7c5a213dd
black4305 <solomon819@naver.com>
2025-01-27 15:09:57 +0900

    1.6 강의 (const)

---
+++ lecture01-6.dart
@@ -0,0 +1,9 @@
+// const : compile-time constant를 만들어줌
+// const -> compile할 때 알고 있는 값에 사용함
+
+void main() {
+    const name = 'nico';
+
+    // final처럼 const도 위에서 한 번 값을 넣었으면 이후 수정 불가
+    name = '니꼬';
+}
