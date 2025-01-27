ad95b284b2b8acb3119c19ed40a629240cf66a79
black4305 <solomon819@naver.com>
2025-01-27 14:32:12 +0900

    1.2 강의

---
+++ lecture01-2.dart
@@ -0,0 +1,12 @@
+// dynamic : 여러 개의 type을 가질 수 있는 변수에 사용하는 키워드
+
+void main{
+    var name;
+    dynamic name2; 
+    // var + 변수명 뒤에 지정 따로 안 하면 dynamic 형태로 알아서 됨
+    // 위 2개 중 원하는 걸로 사용 가능
+
+    name2 = 'nico';
+    name2 = 1;
+    name2 = true;
+}
