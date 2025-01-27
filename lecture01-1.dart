e61c14b5570a838a38781ad51de70161431d582a
black4305 <solomon819@naver.com>
2025-01-27 14:23:06 +0900

    1.1 강의

---
+++ lecture01-1.dart
@@ -0,0 +1,11 @@
+void main() {
+    var name1 = '니꼬';
+    String name2 = 'nico';
+
+    // var + 변수명 = 입력할 값
+    // var을 사용하면 자동으로 type을 결정해줌
+    // 변수 수정 시에는 같은 type으로만 가능
+
+    // 변수 선언 시에는 var을 사용하거나, 직접 type을 명시해주면 됨
+    // 관습적으로 main 함수에선 var을 사용하고, class 안에선 직접 명시해줌
+}
