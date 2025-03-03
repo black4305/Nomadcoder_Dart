d34bca2b7e7a08bdda2d62f50502caf3827a577e
black4305 <solomon819@naver.com>
2025-03-03 17:46:48 +0900

    #3.2 강의 수강 완료

---
+++ lecture03-2.dart
@@ -0,0 +1,9 @@
+/*
+  Parameter 종류
+  1) Positional Parameter
+  - function에 정의된 순서대로 들어감
+  
+  2) Named Parameter
+  - function에 정의된 순서와 상관없이 이름을 통해 값을 전달할 수 있음
+  - NULL Safety 방지를 위해 초기값을 설정(1)하거나 required 키워드를 사용(2)
+*/
